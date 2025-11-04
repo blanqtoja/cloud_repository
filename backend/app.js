f124aefd523f9a3a4da3e68348162e19eecc5c65
Filip Wasiel <filipwasiel@gmail.com>
2025-11-04 11:42:04 +0100

    changed openid-client version to 4

--- backend/app.js
+++ backend/app.js
@@ -1,103 +1,97 @@
-const express = require('express');
-const session = require('express-session');
-const { Issuer, generators } = require('openid-client');
-const app = express();
-
 require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const { Issuer, generators } = require('openid-client')
+const app = express()
 
-let client;
+let client
 // Initialize OpenID Client
 async function initializeClient() {
-    const issuer = await Issuer.discover('https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_eoVPTizeN');
-    client = new issuer.Client({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        redirect_uris: ['https://d84l1y8p4kdic.cloudfront.net'],
-        response_types: ['code']
-    });
-};
-initializeClient().catch(console.error);
+	const issuer = await Issuer.discover('https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_eoVPTizeN')
+	client = new issuer.Client({
+		client_id: process.env.CLIENT_ID,
+		client_secret: process.env.CLIENT_SECRET,
+		redirect_uris: ['https://d84l1y8p4kdic.cloudfront.net'],
+		response_types: ['code'],
+	})
+}
+initializeClient().catch(console.error)
 
-app.use(session({
-    secret: 'some secret',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(
+	session({
+		secret: 'some secret',
+		resave: false,
+		saveUninitialized: false,
+	})
+)
 
 const checkAuth = (req, res, next) => {
-    if (!req.session.userInfo) {
-        req.isAuthenticated = false;
-    } else {
-        req.isAuthenticated = true;
-    }
-    next();
-};
+	if (!req.session.userInfo) {
+		req.isAuthenticated = false
+	} else {
+		req.isAuthenticated = true
+	}
+	next()
+}
 
 app.get('/', checkAuth, (req, res) => {
-    res.render('home', {
-        isAuthenticated: req.isAuthenticated,
-        userInfo: req.session.userInfo
-    });
-});
+	res.render('home', {
+		isAuthenticated: req.isAuthenticated,
+		userInfo: req.session.userInfo,
+	})
+})
 
 app.get('/login', (req, res) => {
-    const nonce = generators.nonce();
-    const state = generators.state();
+	const nonce = generators.nonce()
+	const state = generators.state()
 
-    req.session.nonce = nonce;
-    req.session.state = state;
+	req.session.nonce = nonce
+	req.session.state = state
 
-    const authUrl = client.authorizationUrl({
-        scope: 'phone openid email',
-        state: state,
-        nonce: nonce,
-    });
+	const authUrl = client.authorizationUrl({
+		scope: 'phone openid email',
+		state: state,
+		nonce: nonce,
+	})
 
-    res.redirect(authUrl);
-});
-
+	res.redirect(authUrl)
+})
 
 // Helper function to get the path from the URL. Example: "http://localhost/hello" returns "/hello"
 function getPathFromURL(urlString) {
-    try {
-        const url = new URL(urlString);
-        return url.pathname;
-    } catch (error) {
-        console.error('Invalid URL:', error);
-        return null;
-    }
+	try {
+		const url = new URL(urlString)
+		return url.pathname
+	} catch (error) {
+		console.error('Invalid URL:', error)
+		return null
+	}
 }
 
 app.get(getPathFromURL('https://d84l1y8p4kdic.cloudfront.net'), async (req, res) => {
-    try {
-        const params = client.callbackParams(req);
-        const tokenSet = await client.callback(
-            'https://d84l1y8p4kdic.cloudfront.net',
-            params,
-            {
-                nonce: req.session.nonce,
-                state: req.session.state
-            }
-        );
+	try {
+		const params = client.callbackParams(req)
+		const tokenSet = await client.callback('https://d84l1y8p4kdic.cloudfront.net', params, {
+			nonce: req.session.nonce,
+			state: req.session.state,
+		})
 
-        const userInfo = await client.userinfo(tokenSet.access_token);
-        req.session.userInfo = userInfo;
+		const userInfo = await client.userinfo(tokenSet.access_token)
+		req.session.userInfo = userInfo
 
-        res.redirect('/');
-    } catch (err) {
-        console.error('Callback error:', err);
-        res.redirect('/');
-    }
-});
-
+		res.redirect('/')
+	} catch (err) {
+		console.error('Callback error:', err)
+		res.redirect('/')
+	}
+})
 
 // Logout route
 app.get('/logout', (req, res) => {
-    req.session.destroy();
-    const logoutUrl = `https://<user pool domain>/logout?client_id=1k4fnaelfkln0pfpmkb28t1v92&logout_uri=<logout uri>`;
-    res.redirect(logoutUrl);
-});
+	req.session.destroy()
+	const logoutUrl = `https://<user pool domain>/logout?client_id=1k4fnaelfkln0pfpmkb28t1v92&logout_uri=<logout uri>`
+	res.redirect(logoutUrl)
+})
 
-
-app.set('view engine', 'ejs');
-app.listen(3000);
+app.set('view engine', 'ejs')
+app.listen(3000)
